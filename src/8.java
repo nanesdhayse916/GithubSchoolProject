1cfcbcb7a7783ab975316a43e7b6a10156124c63
nanesdhayse916 <nanesdhayse916@gmail.com>
2025-03-13 20:47:42 +0000

    Add 8.java

---
+++ src/8.java
@@ -0,0 +1,12 @@
+// Java program to demonstrate use of while loop
+import java.util.*;
+public class WhileLoopExample {
+  public static void main(String[] args) {
+    int x = 5;
+    while (x > 0) {
+      System.out.println("The value of x is: " + x);
+      x--;
+    }
+    System.out.println("Loop terminated");
+  }
+}
