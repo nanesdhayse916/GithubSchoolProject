8e52b165c257b2524483a3e6aabee5ce9e276284
nanesdhayse916 <nanesdhayse916@gmail.com>
2025-03-14 06:05:56 +0000

    Add 9.java

---
+++ src/9.java
@@ -0,0 +1,12 @@
+package com.githubschoolproject;
+
+public class RandomJavaCode {
+  public static void main(String[] args) {
+    int x = 5;
+    if (x == 0) {
+      System.out.println("Hello, world!");
+    } else {
+      System.out.println("Goodbye, world!");
+    }
+  }
+}
