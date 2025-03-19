9b8661ea8895f9b56570821461c16c6b9ee9311b
nanesdhayse916 <nanesdhayse916@gmail.com>
2025-03-19 04:44:15 +0000

    Add 14.java

---
+++ src/14.java
@@ -0,0 +1,10 @@
+import java.util.*;
+
+public class JavaCode {
+    public static void main(String[] args) {
+        // Generate a random number between 1 and 10
+        int randomNumber = (int)(Math.random() * 10 + 1);
+
+        System.out.println("Random number: " + randomNumber);
+    }
+}
