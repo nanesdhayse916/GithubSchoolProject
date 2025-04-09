3707171af561c137f07a641bf6190ea9955b2f43
nanesdhayse916 <nanesdhayse916@gmail.com>
2025-04-09 10:47:08 +0000

    Add 30.java

---
+++ src/30.java
@@ -0,0 +1,17 @@
+public class SchoolProject {
+    public static void main(String[] args) {
+        // Example of an empty comment
+
+        System.out.println("Hello, World!");
+        
+        // Another example with a comment explaining its purpose.
+        /* This is also valid Java code */
+
+        int num = 5;
+        if (num > 0) {
+            System.out.println(num + " is positive.");
+        } else {
+            System.out.println(num + " is not positive.");
+        }
+    }
+}
