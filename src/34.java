febee94515d063f76c00331b5683a5ac24804e49
nanesdhayse916 <nanesdhayse916@gmail.com>
2025-04-25 01:33:54 +0000

    Add 34.java

---
+++ src/34.java
@@ -0,0 +1,15 @@
+public class SimpleJavaProject {
+    public static void main(String[] args) {
+        // Example of Java code to demonstrate basic operations
+        int age = 18;
+        String name = "John Doe";
+
+        System.out.println("Hello, World!");
+        
+        if (age > 18) {
+            System.out.println(name + ", you are an adult.");
+        } else {
+            System.out.println(name + ", you are a minor.");
+        }
+    }
+}
