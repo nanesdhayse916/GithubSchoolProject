0b229858117a39856273ef241401d1af978aae86
nanesdhayse916 <nanesdhayse916@gmail.com>
2025-05-26 09:26:00 +0000

    Add 45.java

---
+++ src/45.java
@@ -0,0 +1,15 @@
+public class MySimpleJava {
+    public static void main(String[] args) {
+        // Simple Java program to demonstrate basic loop and conditionals
+
+        // Loop from 0 to 9
+        for (int i = 0; i <= 9; i++) {
+            System.out.println("Looping: " + i);
+        }
+
+        // Print a message when the condition is true
+        if (i == 5) {
+            System.out.println("The value of 'i' is " + i);
+        }
+    }
+}
